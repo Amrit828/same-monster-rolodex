cfd3b55a77670b7ef1fe422a1230992c156bbef7
Amrit Gopinath <amritgopi06@gmail.com>
2020-08-15 16:34:29 +0530

    Updates

---
+++ precache-manifest.39bad3f1ca47db3ac725b01a09e3602c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6698d4bab2d8c1d70a78a36890a1f978",
+    "url": "/same-monster-rolodex/index.html"
+  },
+  {
+    "revision": "b899f77c4c9ec458f587",
+    "url": "/same-monster-rolodex/static/css/main.2de87b73.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/same-monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/same-monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b899f77c4c9ec458f587",
+    "url": "/same-monster-rolodex/static/js/main.068d7303.chunk.js"
+  },
+  {
+    "revision": "6628b0fa3aea21570f34",
+    "url": "/same-monster-rolodex/static/js/runtime-main.8af7523f.js"
+  }
+]);
